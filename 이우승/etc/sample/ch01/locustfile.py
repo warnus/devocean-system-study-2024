9d691ee7a69830e57a898ad8ef05426c37498111
Victor Lee <lee.victor@sk.com>
2024-06-01 15:50:07 +0900

    [이우승] 스터디 준비중

--- 이우승/etc/sample/ch01/locustfile.py
+++ 이우승/etc/sample/ch01/locustfile.py
@@ -3,13 +3,13 @@ from locust import HttpUser, between, task
 class MyUser(HttpUser):
     wait_time = between(1, 2)
 
-    # @task
-    # def calculate_fibonacci(self):
-    #     self.client.get("/fibonacci/random")
-    
     @task
-    def get_employees(self):
-        self.client.get("/employees/")
+    def calculate_fibonacci(self):
+        self.client.get("/fibonacci/random")
+    
+    # @task
+    # def get_employees(self):
+    #     self.client.get("/employees/")
     
     # @task
     # def get_employees(self):
